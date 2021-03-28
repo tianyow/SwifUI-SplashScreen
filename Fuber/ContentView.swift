cbdc886580a5e9194bb693f7a94c425c6bcf3751
tianyow <tianyow@gmail.com>
2021-03-28 11:48:42 +0800

    Initial Commit

---
+++ Fuber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fuber
+//
+//  Created by TIAN MBPR on 2021/3/28.
+//  Copyright © 2021 PSC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
